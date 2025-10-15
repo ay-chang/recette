7d4044bebd63d33dfb3ab7c794d14d2d3e56fa59
AllenChang7 <allenychang2003@gmail.com>
2025-10-15 17:08:05 -0400

    update: removed display options

--- frontend-ios/views/RecipeList/MenuBar.swift
+++ frontend-ios/views/RecipeList/MenuBar.swift
@@ -28,23 +28,23 @@ struct MenuBar: View {
             Spacer()
 
             // Toggle list/grid view
-            HStack {
-                Button(action: {
-                    isListView = true
-                }) {
-                    Image(systemName: "list.bullet")
-                        .font(.title2)
-                        .foregroundColor(isListView ? Color.gray : Color.gray.opacity(0.5))
-                }
-                
-                Button(action: {
-                    isListView = false
-                }) {
-                    Image(systemName: "square.grid.2x2")
-                        .font(.title2)
-                        .foregroundColor(isListView ? Color.gray.opacity(0.5) : Color.gray)
-                }
-            }
+//            HStack {
+//                Button(action: {
+//                    isListView = true
+//                }) {
+//                    Image(systemName: "list.bullet")
+//                        .font(.title2)
+//                        .foregroundColor(isListView ? Color.gray : Color.gray.opacity(0.5))
+//                }
+//                
+//                Button(action: {
+//                    isListView = false
+//                }) {
+//                    Image(systemName: "square.grid.2x2")
+//                        .font(.title2)
+//                        .foregroundColor(isListView ? Color.gray.opacity(0.5) : Color.gray)
+//                }
+//            }
 
         }
         .padding(.bottom)
