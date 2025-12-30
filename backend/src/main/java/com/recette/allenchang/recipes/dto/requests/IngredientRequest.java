4a8179dc2d6305588e5b72528317f375e90cd95f
AllenChang7 <allenychang2003@gmail.com>
2025-12-31 00:22:26 +0100

    refactor: CRUD operations for recipes

--- backend/src/main/java/com/recette/allenchang/recipes/dto/requests/IngredientRequest.java
+++
@@ -1,4 +0,0 @@
-package com.recette.allenchang.recipes.dto;
-
-public record IngredientRequest(String name, String measurement) {
-}
--- backend/src/main/java/com/recette/allenchang/recipes/dto/responses/IngredientResponse.java
+++
@@ -1,4 +0,0 @@
-package com.recette.allenchang.recipes.dto.responses;
-
-public record IngredientResponse(String name, String measurement) {
-}
